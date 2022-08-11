e28b86890cf05fd46e64af09248a43b1568c5eb0
agung10 <agungmubarok24@gmail.com>
2022-08-11 14:35:24 +0700

    Styling Component

--- App.js
+++ App.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
-import { Text, View, Image, TextInput } from "react-native";
+import { Text, View, Image, TextInput, StyleSheet } from "react-native";
+import macbook from "./assets/macbook.png";
 
 // Functional Function (Arrow Function)
 const App = () => {
+  return <StylingComponent />;
+};
+
+const StylingComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "#0abde3",
+          borderWidth: 2,
+          borderColor: "#5f27cd",
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+
+      <Text style={styles.text}>Example Design</Text>
+      <View
+        style={{
+          padding: 12,
+          backgroundColor: "#f2f2f2",
+          width: 212,
+          borderRadius: 8,
+        }}
+      >
+        <Image
+          source={macbook}
+          style={{ width: 188, height: 107, borderRadius: 8 }}
+        />
+        <Text style={{ fontSize: 14, fontWeight: "bold", marginTop: 16 }}>
+          Macbook Air 2021 M1 Mgn63ida
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: "bold",
+            marginTop: 12,
+            color: "#f2994a",
+          }}
+        >
+          Rp 16.699.000
+        </Text>
+        <Text style={{ fontSize: 12, fontWeight: "300", marginTop: 12 }}>
+          Tokopedia
+        </Text>
+        <View style={{backgroundColor: '#6fcf97', paddingVertical: 6, borderRadius: 25, marginTop: 15}}>
+          <Text style={{fontSize: 14, fontWeight: '600', color: 'white', textAlign: 'center'}}>Beli</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#5f27cd",
+    marginTop: 40,
+    marginLeft: 20,
+  },
+});
+
+// Dari pembelajaran ke 8 dan 9
+const SampleComponent = () => {
   return (
     <View>
       <Text>
@@ -19,6 +88,7 @@ const App = () => {
     </View>
   );
 };
+// End Sample Component
 
 const World = () => {
   return <Text>World</Text>;
--- readme.txt
+++ readme.txt
@@ -3,3 +3,6 @@ Video 8 - Membuat Component dengan Functional Component (Hooks) , Implementasi V
 
 Video 9 - Membuat Component dengan Class Component di React Native
     Link: https://www.youtube.com/watch?v=KYTw1RBCZW0&list=PLU4DS8KR-LJ3SP3PpRb870UoT_0_rjLpV&index=9
+
+Video 10 - Basic Styling React Native Component - (Dimensi, Background, Color, Margin, Padding, FontSize)
+    Link: https://www.youtube.com/watch?v=0YfdpjgBa6w&list=PLU4DS8KR-LJ3SP3PpRb870UoT_0_rjLpV&index=10
